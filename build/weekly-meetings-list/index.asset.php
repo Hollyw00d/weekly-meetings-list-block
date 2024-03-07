db4ee3a6096f54b6ccba7d4aeab113e1ec997be8
Matt Jennings <matt@mattjennings.net>
2024-03-06 16:40:38 -0800

    got rid of unneeded code and built code

--- build/weekly-meetings-list/index.asset.php
+++ build/weekly-meetings-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '19e6b82b2c8f9e2d54cb');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2160fb837856b64f93af');
